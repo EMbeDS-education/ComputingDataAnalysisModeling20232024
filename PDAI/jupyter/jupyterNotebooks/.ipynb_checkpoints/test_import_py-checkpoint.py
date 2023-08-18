298c03f2b294d101eebde7a7d3f933a87738286a
Andrea Vandin <andrea.vandin84@gmail.com>
2023-08-18 09:43:23 +0200

    clean

--- PDAI/jupyter/jupyterNotebooks/.ipynb_checkpoints/test_import_py-checkpoint.py
+++
@@ -1,7 +0,0 @@
-#This function does not return any value
-#  It is a void function (no return value)
-#It prints something on display
-def my_function():
-    print('Successfully imported!')
-
-#my_function()
